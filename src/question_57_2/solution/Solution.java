1c09a51198a0a99a99a8acaf3865d629a038bb17
hmq201211 <879602728@qq.com>
2020-08-26 17:47:58 +0800

    826

--- src/question_57_2/solution/Solution.java
+++ src/question_57_2/solution/Solution.java
@@ -11,6 +11,23 @@ import java.util.List;
 public class Solution {
     public int[][] findContinuousSequence(int target) {
         List<int[]> toReturn = new ArrayList<>();
-
+        int l = 1;
+        int r = 1;
+        int sum = 0;
+        while (l <= target / 2) {
+            if (sum == target) {
+                int[] answer = new int[r - l];
+                for (int t = l; t < r; t++) {
+                    answer[t - l] = t;
+                }
+                toReturn.add(answer);
+                sum -= l++;
+            } else if (sum > target) {
+                sum -= l++;
+            } else {
+                sum += r++;
+            }
+        }
+        return toReturn.toArray(new int[toReturn.size()][]);
     }
 }
