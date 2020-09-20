03bc756230cd9d1a030912c02183f843156509de
hmq201211 <879602728@qq.com>
2020-09-20 12:57:04 +0800

    920

---
+++ src/question_12/solutilon/Solution.java
@@ -0,0 +1,18 @@
+package question_12.solutilon;
+
+import java.awt.font.ImageGraphicAttribute;
+
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        char[] target = word.toCharArray();
+        final int height = board.length;
+        if (height==0|| board[0]==null)
+            return false;
+        final int weight = board[0].length;
+        if (weight ==0 || )
+            return false
+    }
+    private boolean dfs(char[] board, int i, int j ,char[] target, int index){
+
+    }
+}
--- src/question_7/solution/Solution.java
+++ src/question_7/solution/Solution.java
@@ -3,41 +3,37 @@ package question_7.solution;
 import common.TreeNode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder == null || inorder == null || preorder.length == 0 || inorder.length == 0)
             return null;
-        TreeNode root = new TreeNode(preorder[0]);
-        int index = splitTree(inorder, preorder[0], 0, inorder.length - 1);
-        if (index > 0) {
-            root.left = buildTree(preorder, inorder, index, index, preorder.length - index - 1, true);
-            root.right = buildTree(preorder, inorder, index, index, preorder.length - index - 1, false);
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        final int length = inorder.length;
+        for (int i = 0; i < inorder.length; i++) {
+            indexMap.put(inorder[i], i);
         }
-        return root;
+        return buildTree(preorder, 0, length - 1, inorder, 0, length - 1, indexMap);
     }
 
-    public TreeNode buildTree(int[] preorder, int[] inorder, int index, int leftTreeSize, int rightTreeSize, boolean isLeft) {
-        if (size == 0)
+    private TreeNode buildTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> indexMap) {
+        if (preStart > preEnd) {
             return null;
-        if (size == 1) {
-            return new TreeNode(inorder[isLeft ? index - 1 : index + 1]);
         }
-        int split = splitTree(inorder, preorder[0]);
-        if (index > 0) {
-            root.left = buildTree(preorder, inorder, index, index, true);
-            root.left = buildTree(preorder, inorder, index, preorder.length - index - 1, false);
+        TreeNode treeNode = new TreeNode(preorder[preStart]);
+        if (preStart != preEnd) {
+            int inorderIndex = indexMap.get(preorder[preStart]);
+            int leftSize = inorderIndex - inStart;
+            int rightSize = inEnd - inorderIndex;
+            TreeNode left = buildTree(preorder, preStart + 1, preStart + leftSize, inorder, inStart, inorderIndex - 1, indexMap);
+            TreeNode right = buildTree(preorder, preEnd - rightSize + 1, preEnd, inorder, inorderIndex + 1, inEnd, indexMap);
+            treeNode.left = left;
+            treeNode.right = right;
         }
+        return treeNode;
     }
 
-    private int splitTree(int[] nums, int target, int start, int end) {
-        int index = -1;
-        for (int i = start; i <= end; i++) {
-            if (nums[i] == target) {
-                index = i;
-                break;
-            }
-        }
-        return index;
-    }
+
 }
